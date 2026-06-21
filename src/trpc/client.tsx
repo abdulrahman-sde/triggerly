11062fd70ec36139062dd4ec8537aa919045ae7b
Abdul Rahman <mrrehman002@gmail.com>
2026-06-21 15:41:58 +0500

    fix(superjson): add superjson json to client

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -3,6 +3,7 @@
 import type { QueryClient } from "@tanstack/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
+import superjson from "superjson";
 import { createTRPCContext } from "@trpc/tanstack-react-query";
 import { useState } from "react";
 import { makeQueryClient } from "./query-client";
@@ -45,7 +46,7 @@ export function TRPCReactProvider(
     createTRPCClient<AppRouter>({
       links: [
         httpBatchLink({
-          // transformer: superjson, <-- if you use a data transformer
+          transformer: superjson,
           url: getUrl(),
         }),
       ],
