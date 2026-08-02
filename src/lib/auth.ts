788f520c3ef861673405897410eca5e51a8edc78
Abdul Rahman <mrrehman002@gmail.com>
2026-08-02 15:46:43 +0500

    fix:replace dev product id with the production one

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,7 @@ import { polarClient } from "@/lib/polar";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/prisma";
+
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
@@ -19,7 +20,7 @@ export const auth = betterAuth({
         checkout({
           products: [
             {
-              productId: "5045f3de-b685-4a97-b8c9-9d30c7610307",
+              productId: "7214d5f4-39aa-4c4e-93e5-ed438d8cc151",
               slug: "Visual-Workflow-Automation",
             },
           ],
